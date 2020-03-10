3658f72847e8f5d588b7291eeca8f2f45fec6b33
francoisvaillant <frvaillant@gmail.com>
2020-03-10 10:36:22 +0100

    feat : avancee des cards trajets

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 
-var map = L.map('map1').setView([51.505, -0.09], 13);
+var map = L.map('map1').setView([44.8400,-0.5530], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+
+var map2 = L.map('map2').setView([44.8400,-0.5530], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map2);
+
+
+var map3 = L.map('map3').setView([44.8400,-0.5530], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map3);
