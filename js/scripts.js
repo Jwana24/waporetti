93b7ef3d62845cd1feec7f46ab3d99cf7221849c
francoisvaillant <frvaillant@gmail.com>
2020-03-10 13:32:27 +0100

    feat : add tables in all cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 
 var map = L.map('map1').setView([44.8400,-0.5530], 13);
 
-L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
@@ -15,8 +14,7 @@ L.geoJSON(firstWay, {
 
 var map2 = L.map('map2').setView([44.8400,-0.5530], 13);
 
-L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 }).addTo(map2);
 
 L.geoJSON(secondWay, {
@@ -26,8 +24,7 @@ L.geoJSON(secondWay, {
 
 var map3 = L.map('map3').setView([44.8400,-0.5530], 13);
 
-L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 }).addTo(map3);
 
 L.geoJSON(thirdWay, {
