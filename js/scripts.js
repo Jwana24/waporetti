7b682fd2fbbc70e72c889c022e533c7266b6ba97
francoisvaillant <frvaillant@gmail.com>
2020-03-10 17:05:15 +0100

    feat : starting burger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,44 @@
+// Show an element
+var show = function (elem) {
+	elem.style.display = 'block';
+};
 
+// Hide an element
+var hide = function (elem) {
+	elem.style.display = 'none';
+};
+
+// Toggle element visibility
+var toggle = function (elem) {
+	// If the element is visible, hide it
+	if (window.getComputedStyle(elem).display === 'block') {
+		hide(elem);
+		return;
+	}
+	// Otherwise, show it
+	show(elem);
+};
+
+document.addEventListener('click', function (event) {
+
+	// Make sure clicked element is our toggle
+	if (!event.target.classList.contains('toggle')) return;
+
+	// Prevent default link behavior
+	event.preventDefault();
+
+	// Get the content
+	var content = document.querySelector(event.target.hash);
+	if (!content) return;
+
+	// Toggle the content
+	toggle(content);
+
+}, false);
+
+
+//////////////////////////////////////////////////////////////////
+// CARTES ////////////////////////////////////////////////////////
 // CARTE 1 ///////////////////////////////////////////////////////
 
 ///////// Initialize the map
