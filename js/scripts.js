979cdfff45af2d660e786e21920e7b9740707b78
francoisvaillant <frvaillant@gmail.com>
2020-03-11 21:12:03 +0100

    feat : map's markers popup fixed

--- js/scripts.js
+++ js/scripts.js
@@ -55,9 +55,9 @@ var myStyle = {
 
 var orangeIcon = L.icon({
     iconUrl: 'images/morange.svg',
-    iconSize:     [38, 60], // size of the icon
-    iconAnchor:   [19, 60], // point of the icon which will correspond to marker's location
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    iconSize:     [38, 95], // size of the icon
+    iconAnchor:   [19, 60],  // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
 });
 
 /////////  Add way to map
@@ -91,9 +91,8 @@ var myStyle2 = {
 var blueIcon = L.icon({
     iconUrl: 'images/mblue.svg',
     iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
     iconAnchor:   [19, 60],  // point of the icon which will correspond to marker's location
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
 });
 
 /////////  Add way to map
@@ -103,13 +102,16 @@ L.geoJSON(secondWay, {
 
 /////////  add markers to map
 var marker21 = L.marker([44.843394, -0.568972], {icon:blueIcon}).addTo(map2);
-marker21.bindPopup("<b>Stalingrad</b>");
+marker21.bindPopup("<b>Bourse</b>");
 
 var marker22 = L.marker([44.85021, -0.563307], {icon:blueIcon}).addTo(map2);
-marker22.bindPopup("<b>Stalingrad</b>");
+marker22.bindPopup("<b>La guiguette</b>");
 
 var marker23 = L.marker([44.862135, -0.548716], {icon:blueIcon}).addTo(map2);
-marker23.bindPopup("<b>Stalingrad</b>");
+marker23.bindPopup("<b>Cité du vin</b>");
+
+var marker24 = L.marker([44.841325, -0.563221], {icon:blueIcon}).addTo(map2);
+marker24.bindPopup("<b>Stalingrad</b>");
 
 
 
@@ -131,17 +133,25 @@ var myStyle3 = {
 var pinkIcon = L.icon({
     iconUrl: 'images/mpink.svg',
     iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
     iconAnchor:   [19, 60],  // point of the icon which will correspond to marker's location
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
 });
 
 L.geoJSON(thirdWay, {
   style: myStyle3
 }).addTo(map3);
 
-L.marker([44.84163,-0.562963], {icon:pinkIcon}).addTo(map3);
-L.marker([44.832295,-0.548748], {icon:pinkIcon}).addTo(map3);
-L.marker([44.826504,-0.546623], {icon:pinkIcon}).addTo(map3);
-L.marker([44.818704,-0.530788], {icon:pinkIcon}).addTo(map3);
-L.marker([44.799066,-0.528191], {icon:pinkIcon}).addTo(map3);
+var marker31 = L.marker([44.84163,-0.562963], {icon:pinkIcon}).addTo(map3);
+marker31.bindPopup("<b>Stalingrad</b>");
+
+var marker32 = L.marker([44.832295,-0.548748], {icon:pinkIcon}).addTo(map3);
+marker32.bindPopup("<b>Floirac</b>");
+
+var marker33 = L.marker([44.826504,-0.546623], {icon:pinkIcon}).addTo(map3);
+marker33.bindPopup("<b>MECA</b>");
+
+var marker34 = L.marker([44.818704,-0.530788], {icon:pinkIcon}).addTo(map3);
+marker34.bindPopup("<b>Bouliac</b>");
+
+var marker35 = L.marker([44.799066,-0.528191], {icon:pinkIcon}).addTo(map3);
+marker35.bindPopup("<b>Bègles</b>");
